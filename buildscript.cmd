eefad9775ebf22085c95b9b4e7c590784a82967a
Benedict Sondershaus <bergben44@gmail.com>
2024-06-14 19:56:45 +0200

    better accessibility for windows buildscript

--- buildscript.cmd
+++ buildscript.cmd
@@ -1,2 +1,2 @@
-g++ -std=c++17 main.cpp gfx\*.c gfx\*.h -I.\SDL2-2.26.4\x86_64-w64-mingw32\include -L.\SDL2-2.26.4\x86_64-w64-mingw32\lib -Wall -lmingw32 -lSDL2main -lSDL2 -I.\gfx -o maincc
+g++ -std=c++17 main.cpp gfx\*.c gfx\*.h -I.\SDL2\x86_64-w64-mingw32\include -L.\SDL2\x86_64-w64-mingw32\lib -Wall -lmingw32 -lSDL2main -lSDL2 -I.\gfx -o maincc
 .\maincc.exe
